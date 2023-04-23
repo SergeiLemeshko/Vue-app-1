a7ed972916601cdb4ef5f134660f2ca83aa97e5a
unknown <lemexaxa@gmail.com>
2023-04-23 21:19:30 +0300

    adding logic to form 1

--- src/views/FormCompany.vue
+++ src/views/FormCompany.vue
@@ -1,13 +1,16 @@
 <template>
 	<!--форма 1 -->
-	<form class="form-tel" action="" method="GET">
-		<input class="form-tel__input" type="tel" placeholder="Введите номер телефона">
-		<button class="form-tel__btn" type="submit"><span>Получить код</span></button>
-		<p class="form-tel__confirm">“Смс с кодом подтверждения отправлено”</p>
-		<input class="form-tel__password" type="password" placeholder="Введите код из смс">
+	<form class="form-tel" action="" method="GET" v-show="!isHideForm1">
+		<div v-if="!isHidePartForm1">
+			<input class="form-tel__input" v-model="inpValue" type="tel" placeholder="Введите номер телефона">
+			<button class="form-tel__btn" @click="changeFormTel" type="submit"><span>Получить код</span></button>
+		</div>
+			<p class="form-tel__confirm" v-if="isHideTel">“Смс с кодом подтверждения отправлено”</p>
+			<input class="form-tel__password" v-model="inpValuePass" v-if="isHidePass" type="password" placeholder="Введите код из смс">
+			<p class="form-tel__sms" v-if="isHideSMS">А вот и ваш пароль из SMS: {{ this.randomNum }}</p>		
 	</form>
 	<!--форма 2 -->
-	<form class="form-profile" action="" method="GET">
+	<form class="form-profile" isHide="false" action="" method="GET" v-show="isHideForm2">
 		<p class="form-profile__request">Пожалуйста, заполните анкету:</p>
 		<input class="form-profile__name" type="text" placeholder="Введите имя">
 		<input class="form-profile__surname" type="text" placeholder="Введите фамилию"> 
@@ -34,6 +37,47 @@
 
 export default {
 	name: "FormCompany",
+	props: {
+			isHideForm1: Boolean,
+			isHidePartForm1: Boolean,
+			isHideForm2: Boolean,
+			isHideTel: Boolean,
+			isHidePass: Boolean,
+			randomNum: Number,
+			inpValue: Number,
+			isHideSMS: Boolean,
+			inpValuePass: Number,
+	},
+	data() {
+		return {
+			isHideForm1: false,
+			isHidePartForm1: false,
+			isHideForm2: false,
+			isHideTel: false,
+			isHidePass: false,
+			randomNum: 0,
+			inpValue: 0,
+			isHideSMS: false,
+			inpValuePass: 0,
+		}
+	},
+	methods: {
+		changeFormTel() {
+			this.findInpValue();
+			this.isHidePartForm1 = true;
+			this.isHideTel = true;
+			setTimeout(() => {
+			this.isHideTel = false;
+			this.isHidePass = true; 
+			this.isHideSMS = true;
+		}, 2000 );
+	},
+		findInpValue(min, max) {
+			min = this.inpValue[2] + this.inpValue[3];
+			max = this.inpValue[1] + this.inpValue[4];
+			return this.randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+		},
+	}
 }
 
 </script>
@@ -56,6 +100,8 @@ export default {
 .form-tel__confirm {}
 
 .form-tel__password {}
+
+.form-tel__sms{}
 
 /*вторая форма*/
 .form-profile {
